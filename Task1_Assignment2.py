1ca7fd9e87d61b8bed5094c34ac973daed3e5aea
Hariom738 <harigamer85@gmail.com>
2026-01-17 13:12:28 +0530

    Add files via upload

---
+++ Task1_Assignment2.py
@@ -0,0 +1,2 @@
+num1 = int(input("Enter a number: "))
+print("Even") if num1 % 2 == 0 else print("Odd")
---
+++ Task2_Assignment2.py
@@ -0,0 +1,7 @@
+for i in range(1,2):
+
+
+
+ print(f"The sum of all numbers from 1 to 50 is: {sum(range(1,51))}")
+
+
